61f5bbcac6e6cee7c4ccd9f2d8ceb56e2b6a9fd8
GamingLuke1337 <64887613+GamingLuke1337@users.noreply.github.com>
2025-01-10 11:12:56 +0100

    fxmanifest.lua cleanup

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,13 +1,8 @@
 fx_version 'cerulean'
-game 'gta5'
+games {'gta5', 'rdr3'}
 author 'atiysu'
 lua54 'yes'
 discord 'https://discord.gg/dvPMYsRFNx'
-
-games {
-  "gta5",
-  "rdr3"
-}
 
 shared_scripts{
   "shared/*.lua",
@@ -17,16 +12,3 @@ client_scripts {
   "client/utils.lua",
   "client/*.lua",
 }
-
-local isEscrowed = false
-if isEscrowed then
-  escrow_ignore {
-    "shared/*.lua",
-    "client/open.lua",
-  }
-else
-  escrow_ignore {
-    "shared/*.lua",
-    "client/**/*",
-  }
-end
